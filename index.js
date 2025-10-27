2561dce5ede84d14c9ffd18b4bc6299d4cb3cad0
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-27 12:11:01 +0700

    menambah sistem app get pada index.js

--- index.js
+++ index.js
@@ -33,3 +33,11 @@ app.post("/komik", async(req,res)=>{
     }
 })
 
+app.get("/komik", async(req,res)=>{
+    try{
+        const komik = await db.komik.findAll();
+        res.send(komik);
+    } catch(err){
+        res.send(err);
+    }
+})
