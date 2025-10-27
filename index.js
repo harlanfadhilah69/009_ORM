9e23040ac44feab98de00fc4e547da9bdcf4f39e
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-27 12:10:37 +0700

    menambah sistem app post pada index.js

--- index.js
+++ index.js
@@ -22,3 +22,14 @@ db.sequelize.sync().then((result) => {
     .catch((err) => {
         console.log(err);
 });
+
+app.post("/komik", async(req,res)=>{
+    const data = req.body;
+    try{
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch(err){
+        res.send(err);
+    }
+})
+
